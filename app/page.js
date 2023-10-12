3a46cf656d4cdc8c7bcadd5bc934222c3d3ba6df
Drakkarrr <junrey404@gmail.com>
2023-10-13 05:18:30 +0800

    added form and label component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return <Button className='custom-bg'>Click me</Button>;
 }
